a6f2944a5a09aa6c3802ebba3ea26779d76e8dd6
Abhijit Dey <87671060+Avichiii@users.noreply.github.com>
2023-05-14 22:20:00 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
 PATH = r'C:\Socket Programming\file_server'
 for i in range(10000):
     with open(f'{PATH}\\file2.txt', 'a') as file:
-        file.write('this is sparta.\n')
+        file.write('this is test file content.\n')
